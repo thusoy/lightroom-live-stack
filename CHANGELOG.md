7349cfb9d9ead7bf4f90515fd46017f5ce3aebcb
Tarjei Husøy <git@thusoy.com>
2020-10-18 14:25:54 -0700

    Add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+Changelog
+=========
+
+Notable changes to the project will be added here.
+
+
+# 1.0.0 - 2020-08-01
+
+First release!
--- tools/build.sh
+++ tools/build.sh
@@ -3,7 +3,7 @@
 rm -rf dist
 mkdir dist
 cp -r LiveStack.lrdevplugin dist/LiveStack.lrplugin
-cp LICENSE README.md dist/LiveStack.lrplugin
+cp CHANGELOG.md LICENSE README.md dist/LiveStack.lrplugin
 cd dist
 zip -r LiveStack.zip LiveStack.lrplugin
 rm -rf LiveStack.lrplugin
